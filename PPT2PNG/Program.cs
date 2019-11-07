fde5213d77f7cba8b4b33b031b46fa0ff8d6bf50
tobi <20150562@htlmoedling.onmicrosoft.com>
2019-11-07 19:30:25 +0100

    Support for ppt files

--- PPT2PNG/Program.cs
+++ PPT2PNG/Program.cs
@@ -37,19 +37,35 @@ namespace PPT2PNG
                 Console.WriteLine(args[0]);
                 
                 var app = new Powerpoint.Application();
-                Powerpoint.Presentation ppt = app.Presentations.Open(args[0]+@"\Datei.pptx");
+                Powerpoint.Presentation ppt;
+
+                try
+                {
+                    ppt = app.Presentations.Open(args[0] + @"\Datei.pptx", Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse);
+                }
+                catch
+                {
+                    try
+                    {
+                        ppt = app.Presentations.Open(args[0] + @"\Datei.ppt", Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("No valid PPT found! 'Datei.ppt', 'Datei.pptx'");
+                        return;
+                    }
+                }
 
                 Console.WriteLine("Starting Converting " + ppt.Path);
 
-                for (int i= 0; i< ppt.Slides.Count; i++)
+                for (int i= 1; i<= ppt.Slides.Count; i++)
                 {
-                    ppt.Slides[i].Export(args[0] + @"\" + i, "PNG");
+                    ppt.Slides[i].Export(args[0] + @"\" + i+ ".png", "PNG");
                 }
+
+                Console.WriteLine("Slides successfully converted");
+                ppt.Close();
             }
-
-            
-
-
         }
     }
 }
