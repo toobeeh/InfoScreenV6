61e776e9337c767160796e6bd63ed9ed0fa8fa4d
tobi <20150562@htlmoedling.onmicrosoft.com>
2019-11-17 00:12:07 +0100

    Added Conversion Service. Current issue on server: insufficient memory

---
+++ PPTConverterService/PPTConverter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using Powerpoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace PPTConverterService
+{
+    public partial class PPTConverter : ServiceBase
+    {
+        public PPTConverter()
+        {
+            InitializeComponent();
+        }
+
+        private string directory;
+
+        protected override void OnStart(string[] args)
+        {
+            //eventLog.WriteEntry("PowerPoint to PNG Converter has started and will supervise the infoscreen presentation directories D:/infoscreen-publish/Screen/presentations");
+            directory = @"C:\Users\Tobi\Desktop\ppt";
+
+            Timer pollingDirectoryTimer = new Timer()
+            {
+                Interval = 100000
+            };
+
+            pollingDirectoryTimer.Elapsed += new ElapsedEventHandler(this.CheckDirectoryTree);
+            pollingDirectoryTimer.Start();
+
+        }
+
+        protected override void OnStop()
+        {
+            //eventLog.WriteEntry("PowerPoint to PNG Converter has been stopped.");
+        }
+
+        private void CheckDirectoryTree(object sender, ElapsedEventArgs args)
+        {
+            // search for "Datei" ppt / pptx files in directory
+            List<string> powerpointFiles = GetDirectoryPowerpoints(directory);
+
+            // convert all files 
+            foreach (string file in powerpointFiles)
+            {
+                ConvertPowerpoint(file);
+            }
+        }
+
+        private List<string> GetDirectoryPowerpoints(string searchDirectory)
+        {
+            List<string> powerpoints = new List<string>();
+
+            foreach (string file in Directory.GetFiles(searchDirectory))
+            {
+                if (Path.GetFileName(file) == "Datei.ppt" || Path.GetFileName(file) == "Datei.pptx") powerpoints.Add(file);
+            }
+
+            if(Directory.GetDirectories(searchDirectory).Length > 0)
+            {
+                foreach (string subSearchDirectory in Directory.GetDirectories(searchDirectory))
+                {
+                    powerpoints.AddRange(GetDirectoryPowerpoints(subSearchDirectory));
+                }
+            }
+
+            return powerpoints;
+        }
+
+        private void ConvertPowerpoint(string file)
+        {
+            //EventLog.WriteEntry("Initiating Conversion of " + file);
+            Powerpoint.Application app;
+            Powerpoint.Presentation ppt;
+            app = new Powerpoint.Application();
+
+            try
+            {
+                //EventLog.WriteEntry("Looking for Datei.ppt");
+                ppt = app.Presentations.Open(file, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoFalse);
+            }
+            catch
+            {
+                //EventLog.WriteEntry("PPT not valid or MS Office is not installed!");
+                return ;
+            }
+            
+            //EventLog.WriteEntry("Converting " + ppt.Path);
+
+            for (int i = 1; i <= ppt.Slides.Count; i++)
+            {
+                ppt.Slides[i].Export(Path.GetDirectoryName(file) + @"\" + i + ".png", "PNG");
+            }
+
+            //EventLog.WriteEntry("Slides successfully converted");
+            ppt.Close();
+            //File.Delete(file);
+        }
+
+    }
+}
---
+++ verwaltung_code/PowerpointConverterService/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PowerpointConverterService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.User;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.ServiceName = "PPT Polling Converter ISv6";
+            this.serviceInstaller1.Description = "Converts all Powerpoints in infoscreen presentations";
+            this.serviceInstaller1.DisplayName = "PPTConvertService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
