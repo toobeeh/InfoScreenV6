4eb4929b0c90fb3da998dc0be4d17de2acf90e4a
Tobias Scharsching <20150562@htlmoedling.onmicrosoft.com>
2019-12-08 23:48:46 +0100

    Added powerpoint razorpage

---
+++ ScreenCoreApp/Pages/ContentPages/PowerPoint.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScreenCoreApp.Pages.ContentPages
+{
+    public class PowerPointModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
