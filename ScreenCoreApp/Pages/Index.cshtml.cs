104715c728a548f59999cc88360f86bf81d7b501
Tobias Scharsching <20150562@htlmoedling.onmicrosoft.com>
2019-12-11 18:59:05 +0100

    Optimized to be deployable on InfoScreen IIS Server

--- ScreenCoreApp/Pages/Index.cshtml.cs
+++ ScreenCoreApp/Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Infoscreen_Verwaltung.classes;
+using System.IO;
 
 namespace ScreenCoreApp.Pages
 {
@@ -12,7 +14,43 @@ namespace ScreenCoreApp.Pages
         public void OnGet()
         {
             Screen.CheckScreenID(Request.Query, HttpContext);
-            ViewData["ID"] = "Eingegebene ID per QS:" + Screen.GetSessionScreenID(HttpContext);
+            ViewData["Handler"] = "Default";
+
+            PreloadPowerpoints();
+        }
+
+        private List<string> PptPicturePaths;
+
+        private void PreloadPowerpoints()
+        {
+            PptPicturePaths = new List<string>();
+
+            int screenID = Screen.GetSessionScreenID(HttpContext);
+            if (screenID < 1) return;
+
+            string departmentName = DatenbankAbrufen.BildschirmInformationenAbrufen(screenID).Abteilung;
+
+            int modeID = DatenbankAbrufen.AktuellenBetriebsmodeAbrufen(departmentName);
+
+            int powerpointID = DatenbankAbrufen.AnzuzeigendeElemente(screenID.ToString()).PowerPoints;
+            if (powerpointID == -1) return;
+
+            string presentationRoot = Path.Combine(new string[] { @"D:\infoscreen_publish\Screen\Presentations\", modeID.ToString(), powerpointID.ToString() });
+
+            PptPicturePaths = Directory.GetFiles(presentationRoot, "*.png").ToList();
+
+        }
+
+        public string GeneratePreloadHtmlCode() // should be called on startup and generate preload table
+        {
+            string preloadMarkup = "";
+
+            PptPicturePaths.ForEach((string path) =>
+            {
+                preloadMarkup += "<td id='" + Path.GetFileNameWithoutExtension(path) + "> <image src=" + path + "> </td>";
+            });
+
+            return preloadMarkup;
         }
     }
 }
---
+++ ScreenCoreApp/obj/Release/netcoreapp2.1/PubTmp/Out/wwwroot/JS/Index.js
@@ -0,0 +1,38 @@
+﻿
+var queryString = window.location.search;
+
+
+
+window.onload = function () {
+    let viewFrame = document.getElementById("view_frame");
+    let preFrame = document.getElementById("preload_frame");
+
+    preFrame.onload = previewLoaded;
+    viewFrame.onload = viewLoaded;
+
+    viewFrame.src = "Init" + queryString;
+
+}
+
+
+function previewLoaded() {
+    let viewFrame = document.getElementById("view_frame");
+    let preFrame = document.getElementById("preload_frame");
+
+    viewFrame.style.display = "none";
+    preFrame.style.display = "block";
+
+    setTimeout(function () { viewFrame.src = viewFrame.src; },5000);
+
+}
+
+function viewLoaded() {
+    let viewFrame = document.getElementById("view_frame");
+    let preFrame = document.getElementById("preload_frame");
+
+    preFrame.style.display = "none"; 
+    viewFrame.style.display = "block";
+
+    setTimeout(function () { preFrame.src = viewFrame.src; }, 5000);
+}
+
